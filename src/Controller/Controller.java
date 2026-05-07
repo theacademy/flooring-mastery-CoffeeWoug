fcf591c4bb3b2fcd8095548f11afd10a2fe70f2c
CoffeeWoug <mihir_mistry@outlook.com>
2026-05-07 10:44:31 +0100

    Added date validation

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -7,6 +7,8 @@ import UI.UserIOConsoleImpl;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.InputMismatchException;
 
 public class Controller {
 
@@ -27,7 +29,7 @@ public class Controller {
                     io.print("Display Orders");
                     break;
                 case 2:
-
+                    addOrder();
                     break;
                 case 3:
                     io.print("Edit an order");
@@ -53,19 +55,29 @@ public class Controller {
     }
 
     public void addOrder() {
-        String userDate = view.getDateFromUser();
-        LocalDate ld = null;
-        try {
-            validateDate(userDate);
-        } catch(DateTimeException dte) {
-            io.print("Not a valid date, please try again");
-        }
+        LocalDate userDate = validateDate();
     }
 
-    public LocalDate validateDate(String date) {
+    // CHECKS IF DATE IS VALID FORMAT AND IS AFTER TODAY
+    public LocalDate validateDate() {
+        boolean dateValidated = false;
         LocalDate todayDate = LocalDate.now();
-        LocalDate ld = LocalDate.parse(date, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
-        System.out.println(ld);
+
+        while(!dateValidated) {
+            // Get String date from User
+            try {
+                String userDate = view.getDateFromUser();
+                LocalDate ld = LocalDate.parse(userDate, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+                if (ld.isAfter(todayDate)) {
+                    System.out.println("Date is after");
+                    dateValidated = true;
+                } else if (ld.isBefore(todayDate)) {
+                    System.out.println("Date is before");
+                }
+            } catch(InputMismatchException | DateTimeException e) {
+                io.print("Not valid date");
+            }
+        }
         return LocalDate.now();
     }
 }
